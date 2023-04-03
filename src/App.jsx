f30545815116c7cbbb2623058e231d45c7351823
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-04-03 07:49:32 -0500

    refactor: add counter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+function Counter({ counter, onClick, className }) {
+  return (
+    <div className={className} onClick={onClick}>
+      <div>{counter}</div>
+    </div>
+  );
+}
 
 function App() {
-  const [counterTeamA, setCounterTeamA] = useState(0)
-  const [counterTeamB, setCounterTeamB] = useState(0)
+  const [counterTeamA, setCounterTeamA] = useState(0);
+  const [counterTeamB, setCounterTeamB] = useState(0);
 
   function counterPointsTeamA() {
-    const newPoints = counterTeamA +  1
-    setCounterTeamA(newPoints)
+    const newPoints = counterTeamA + 1;
+    setCounterTeamA(newPoints);
   }
-  
+
   function counterPointsTeamB() {
-    const newPoints = counterTeamB +  1
-    setCounterTeamB(newPoints)
+    const newPoints = counterTeamB + 1;
+    setCounterTeamB(newPoints);
   }
 
   return (
-    <div className='container'>
-      <div className='up' onClick={counterPointsTeamA}>{counterTeamA}</div>
-      <div className='down' onClick={counterPointsTeamB}>{counterTeamB}</div>
+    <div className="container">
+      <Counter
+        counter={counterTeamA}
+        onClick={counterPointsTeamA}
+        className="up"
+      />
+      <Counter
+        counter={counterTeamB}
+        onClick={counterPointsTeamB}
+        className="down"
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
